73039bda3dcea2352850ab68199b9d898f88bd83
Judy <judychue@pursuit.org>
2023-10-26 15:44:12 -0400

    first commit

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,29 @@
+
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links',
+    },
+    {
+        title: 'About',
+        url: '#',
+        cName: 'nav-links',
+    },
+    {
+        title: 'Rewards',
+        url: '#',
+        cName: 'nav-links',
+    },
+    {
+        title: 'Contact',
+        url: '#',
+        cName: 'nav-links',
+    },
+    {
+        title: 'Sign up',
+        url: '#',
+        cName: 'nav-links-mobile',
+    }
+       
+]
