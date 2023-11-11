54393b3b055be4363cf863d8be6225e0add6b021
atiradoGit88 <angeltirado@pursuit.org>
2023-11-11 12:48:39 -0500

    edited connect with us page.

--- src/components/pages/Charities.js
+++ src/components/pages/Charities.js
@@ -5,6 +5,8 @@ import './Charities.css'
 export default function Charities() {
   return (  
   <div className="charities">
+    <div className='suggested-charities'>Suggested <br></br> Charities</div>
+    <br></br>
     <div className='charity-box'>
       <a href='https://secure.aspca.org/donate/ps-gn-p2?ms=MP_PMK_Googlebrand&initialms=MP_PMK_Googlebrand&pcode=WPSN7GO2PK01&lpcode=WPSN7GO1PK01&test&ds_rl=1066461&gad=1&gclid=CjwKCAjw1t2pBhAFEiwA_-A-NOKUGazmuhnQ5UAlcLa5Bq_wZxto2hCL8XhpOu_w3hGw5sBs8y1vYhoCH4cQAvD_BwE&gclsrc=aw.ds'>
         <img src='https://www.animalprotective.org/dev/wp-content/uploads/2016/08/aspca_logo-768x294.jpg' alt='aspca'></img>
@@ -19,17 +21,19 @@ export default function Charities() {
     </div>
     <div className='charity-box'>
       <a href='https://www.redcross.org/donate/donation.html/'>
-        <img src='https://bloximages.newyork1.vip.townnews.com/spiritofomaha.com/content/tncms/assets/v3/editorial/e/97/e972a4be-737c-5737-9b3d-6bf1608d4155/5f241adfa5c01.image.jpg?resize=1200%2C534' alt='red-cross'></img>
+        <img src='https://www.redcross.org/' alt='red-cross'></img>
       </a>
           <p className='mission-statement'>The Red Cross honors donor intent and all donations earmarked for Disaster Relief will be used to help people affected by disasters, big and small. Americans work hard for their money, and we’re committed to being the very best stewards of our donors’ dollar.</p>
     </div>
     <div className='charity-box'>
-      <a href='https://www.unicefusa.org/?form=giveeducation&gad=1&gclid=CjwKCAjw1t2pBhAFEiwA_-A-NBA8dCAGZ4Pdnme7itvudfq1TVnNUwlEYmmoCHYRsYXjJw4OBmiOUhoC3iMQAvD_BwE
-      '>
+      <a href='https://www.unicefusa.org/'>
         <img src='https://images.squarespace-cdn.com/content/v1/56aca675f8baf35958bfaec6/1484432098546-YQO6NJJQBC8506FIN2FH/image-asset.jpeg?format=2500w' alt='unicefusa'></img>
       </a>
           <p className='mission-statement'>Relentlessly Pursuing a Better World for Every Child
 UNICEF has helped save and meaningfully improve more children’s lives than any other humanitarian organization. UNICEF won’t stop until every child is healthy, educated, protected and respected.</p>
+    </div>
+    <div className='donation'>
+      <p>CLICK HERE TO DONATE</p>
     </div>
   </div>
 );
