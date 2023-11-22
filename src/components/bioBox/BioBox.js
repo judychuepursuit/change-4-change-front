72666822ccb5db09913b34b317183df725f9e63b
Sabri Mohiuddin <you@example.com>
2023-11-21 20:53:49 -0500

    make changes

---
+++ src/components/bioBox/BioBox.js
@@ -0,0 +1,32 @@
+import React from "react";
+import './BioBox.css';
+
+const BioBox = (props) => {
+    return (
+        <div className="bio-box">
+          <img
+            src={props.bio.image}
+            alt="bioImage"
+            className="bioImg"
+          ></img>
+
+          <div className="bio-box__header">
+            <div className="bio-box__name">
+              {props.bio.name}
+            </div>
+            <div className="bio-box__links">
+              <a href={props.bio.githubLink}>
+                <img src='./github.png'></img>
+              </a>
+            </div>
+          </div>
+          
+          <p className="about-me">
+            {props.bio.aboutMe}
+          </p>
+        </div>
+      );
+};
+// test
+
+export default BioBox;
