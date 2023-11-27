325a42a7f464b260057363bf7027d6a4a75db958
Judy <judychue@pursuit.org>
2023-11-27 11:34:58 -0500

    edit to hiw

--- src/components/pages/How-It-Works.js
+++ src/components/pages/How-It-Works.js
@@ -6,8 +6,8 @@ export default function HowItWorks() {
   return (  
     <body>
       <div className="how-it-works-page">
-  <h1>how it works</h1><br></br>
       <div className="how-it-works-image"/>
+      <h1>how it works</h1><br></br>
       <div className="text-overlay hiw-header">
       <div className="centered"><b> Rewards Badges & connecting to like minded people in your commnity via your Social media accounts: </b> 
 Simply Register/log-in, select a charity, enter your payment information the frequency and donation amount, then let the fun begin. Each step accrues a user points, those points accrue reward badges.  Paid donation's whether it's a monthly or one time donation earns a user milestones points which a user can chose to connect their social media accounts. These monthly payments, badges and milestones are then shared with your friends/family. The posts/feed helps incentivize others to engagein signing-up and donating through the app. Users get reminders of the donations they make by the posts/feed, show casing points and badges. These short moments of accomplisments are statistically equivalent to producing good endorphins (can be compared to eating chocolate, being in love) the users is also more importantly reminded regularly of the help they are providing to the charities in need and fostering a community of like minded individuals via the connected social media sites.</div>
@@ -18,9 +18,9 @@ Simply Register/log-in, select a charity, enter your payment information the fre
 
 }
 
-{/* <body>
+/* <body>
 <div className="about">
 <div className="text-overlay">
-<h1>about</h1><br></br> */}
+<h1>about</h1><br></br> */
 
 // test
