d0bd67e1177f06891069967f6f5891a5b9caad0a
Sabri Mohiuddin <you@example.com>
2023-12-17 14:37:09 -0500

    merge

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,107 +1,101 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // import { MenuItems } from "./MenuItems"
-import { Link } from 'react-router-dom';
-import './Navbar.css'
-import { Button } from "./Button"
-import Dropdown from './Dropdown';
-
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+import { Button } from "./Button";
+import Dropdown from "./Dropdown";
 
 function Navbar(props) {
-  
-  console.log(props)
+  console.log(props);
 
-    const [click, setClick] = useState(false);
-    const [dropdown, setDropdown] = useState(false);
-  
-    const handleClick = () => setClick(!click);
-    const closeMobileMenu = () => setClick(false);
-  
-    const onMouseEnter = () => {
-      if (window.innerWidth < 960) {
-        setDropdown(false);
-      } else {
-        setDropdown(true);
-      }
-    };
-  
-    const onMouseLeave = () => {
-      if (window.innerWidth < 960) {
-        setDropdown(false);
-      } else {
-        setDropdown(false);
-      }
-    };
-  
-    return (
-      <>
-        <nav className={props.hidden ? 'navbar hidden' : 'navbar'} >
-          <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
-            <h5>change 4 change <i className="fa-solid fa-coins"></i></h5>
-          </Link>
-          <div className='menu-icon' onClick={handleClick}>
-            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
-          </div>
-          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-            <li className='nav-item'>
-              <Link to='/' className='nav-links' onClick={closeMobileMenu}>
-                Home
-              </Link>
-            </li>
-            <li
-              className='nav-item'
-              onMouseEnter={onMouseEnter}
-              onMouseLeave={onMouseLeave}
+  const [click, setClick] = useState(false);
+  const [dropdown, setDropdown] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const onMouseEnter = () => {
+    if (window.innerWidth < 960) {
+      setDropdown(false);
+    } else {
+      setDropdown(true);
+    }
+  };
+
+  const onMouseLeave = () => {
+    if (window.innerWidth < 960) {
+      setDropdown(false);
+    } else {
+      setDropdown(false);
+    }
+  };
+
+  return (
+    <>
+      <nav className={props.hidden ? "navbar hidden" : "navbar"}>
+        <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+          <h5>
+            change 4 change <i className="fa-solid fa-coins"></i>
+          </h5>
+        </Link>
+        <div className="menu-icon" onClick={handleClick}>
+          <i className={click ? "fas fa-times" : "fas fa-bars"} />
+        </div>
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
+          <li className="nav-item">
+            <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+              Home
+            </Link>
+          </li>
+          <li
+            className="nav-item"
+            onMouseEnter={onMouseEnter}
+            onMouseLeave={onMouseLeave}
+          >
+            <Link
+              to="/how-it-works"
+              className="nav-links"
+              onClick={closeMobileMenu}
             >
-              <Link
-                to='/how-it-works'
-                className='nav-links'
-                onClick={closeMobileMenu}
-              >
-                How it works <i className='fas fa-caret-down' />
-              </Link>
-              {dropdown && <Dropdown />}
-            </li>
+              How it works <i className="fas fa-caret-down" />
+            </Link>
+            {dropdown && <Dropdown />}
+          </li>
 
-            <li className='nav-item'>
-              <Link
-                to='/rewards'
-                className='nav-links'
-                onClick={closeMobileMenu}
-              >
-                Rewards
-              </Link>
-            </li>
-            
-            <li className='nav-item'>
-              <Link
-                to='/connect-us'
-                className='nav-links'
-                onClick={closeMobileMenu}
-              >
-                {/* was Contact Us, now connect */}
-                Connect
-              </Link>
-            </li>
+          <li className="nav-item">
+            <Link to="/rewards" className="nav-links" onClick={closeMobileMenu}>
+              Rewards
+            </Link>
+          </li>
 
-            <li>
-              <Link
-                to='/sign-up'
-                className='nav-links-mobile'
-                onClick={closeMobileMenu}
-              >
-                Sign Up
-              </Link>
-            </li>
-          </ul>
-          <Button />
-        </nav>
-      </>
-    );
-  }
-  
-  export default Navbar;
-  
+          <li className="nav-item">
+            <Link
+              to="/connect-us"
+              className="nav-links"
+              onClick={closeMobileMenu}
+            >
+              {/* was Contact Us, now connect */}
+              Connect
+            </Link>
+          </li>
 
+          <li>
+            <Link
+              to="/sign-up"
+              className="nav-links-mobile"
+              onClick={closeMobileMenu}
+            >
+              Sign Up
+            </Link>
+          </li>
+        </ul>
+        <Button />
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
 
 // test
 
@@ -119,7 +113,7 @@ function Navbar(props) {
 //                 <div className="menu-icon" onClick={this.handleClick}>
 //                     <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}>
 //                     </i>
-                    
+
 //                 </div>
 //                 <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
 //                     {MenuItems.map((item, index) => {
